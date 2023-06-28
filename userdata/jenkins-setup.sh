3ceca3b52629c5814570a39f21fa1024ba49f19e
Lior Milliger <64707466+liormilliger@users.noreply.github.com>
2023-06-28 13:24:15 +0300

    Update jenkins-setup.sh
    replacing curl command with updated from jenkins website
    installing openjdk-8-jdk on the server for jenkins configuration for this job

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -9,3 +9,4 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
+sudo apt install openjdk-8-jdk -y
